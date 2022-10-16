a5f684004f1731e183e1f3f229f83fd5cc528f3a
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-16 18:05:16 -0400

    Added comments.

--- assets/game_approval.py
+++ assets/game_approval.py
@@ -2,9 +2,16 @@ from pyteal import *
 
 def game():
     # Write your code here
-    handle_creation = Return(Int(1)) # need this: 5pt health points start / initialize global state of the highest damage dealt to 0
-    handle_optin = Return (Int(1)) 
-    handle_noop = Return(Int(1))
+    handle_creation = Return(Int(1)) # need this:
+                                     # 5pt health points start
+                                     # initialize global state of the highest damage dealt to 0
+    handle_optin = Return (Int(1)) # need this: 
+                                   # initialize local state of damage done to the monster to 0
+                                   # should prevent player from making multiple opt in transactions
+    handle_noop = Return(Int(1))  # need this: 
+                                  #  each attack reduces hit points of monster by 2 
+                                  #  the players local state should keep track of the damage dealt to that monster 
+                                  #  the contract will use the local state value to determine if this player did the most damage to the monster    
     handle_closeout = Return(Int(1))
     handle_updateapp = Return(Int(0))
     handle_deleteapp = Return(Int(1))
