215cea0b818bdde4c0d18f92b2545b53170d8294
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-17 15:19:19 -0400

    Added send algos subroutine to the handle noop condition subroutine.

--- assets/game_approval.py
+++ assets/game_approval.py
@@ -2,16 +2,37 @@ from pyteal import *
 
 def game():
     # Write your code here
-    handle_creation = Return(Int(1)) # need this:
-                                     # 5pt health points start
-                                     # initialize global state of the highest damage dealt to 0
-    handle_optin = Return (Int(1)) # need this: 
-                                   # initialize local state of damage done to the monster to 0
-                                   # should prevent player from making multiple opt in transactions
-    handle_noop = Return(Int(1))  # need this: 
-                                  #  each attack reduces hit points of monster by 2 
-                                  #  the players local state should keep track of the damage dealt to that monster 
-                                  #  the contract will use the local state value to determine if this player did the most damage to the monster    
+    handle_creation = Seq([
+        App.globalPut(Bytes("MonsterName"), Bytes("Scary Monster")),
+        # 5 pt of health to start
+        App.globalPut(Bytes("HitPoints"), Btoi(Bytes("5"))),
+        # initialize global state of the highest damage dealt to 0
+        App.globalPut(Bytes("HighestDamageDealt"), Btoi(Bytes("0"))),
+        Return(Int(1))
+    ])
+        
+    handle_optin = Seq([
+        # should prevent player from making multiple opt in transactions
+        Assert(App.optedIn(Txn.sender(),Txn.application_id())),
+        # initialize local state of damage done to the monster to 0
+        App.localPut(Txn.sender(), Bytes("DamageDone"), Btoi(Bytes("0"))),
+        Return(Int(1))
+    ])
+
+    attack = Seq(
+        #  each attack reduces hit points of monster by 2 
+        App.globalPut(Bytes("HitPoints"), App.globalGet(Bytes("HitPoints"))-2),
+        #  the players local state should keep track of the damage dealt to that monster 
+        App.localPut(Txn.sender(), Bytes("DamageDone"), App.globalGet(Bytes("DamageDone")),
+        Return(Int(1))
+    )
+
+    handle_noop = Cond(
+    #  the contract will use the local state value to determine if this player did the most damage to the monster    
+            [Txn.application_args[0] == Bytes("Attack"), attack],
+            [Txn.application_args[0] == Bytes("SendAlgos"), send_algos],
+    )
+
     handle_closeout = Return(Int(1))
     handle_updateapp = Return(Int(0))
     handle_deleteapp = Return(Int(1))
